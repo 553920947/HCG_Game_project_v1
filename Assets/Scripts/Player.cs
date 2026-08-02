caa38c86e20af0fbde5417c6e0a57801f8f77879
553920947 <553920947@qq.com>
2026-08-02 19:49:25 +0800

    新加游戏脚本

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private int _speed = 500;
+    
+    public void Start()
     {
-        
+        Debug.Log("测试游戏开始。");  
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    public void Update()
     {
-        
+        Debug.Log("测试游戏更新。");
     }
 }
