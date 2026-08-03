9ee8e3541db17c8976e50e35bdc85d77c8967312
553920947 <553920947@qq.com>
2026-08-03 18:42:07 +0800

    更新玩家物体左右控制和移动

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private int _speed = 500;
+    [SerializeField] private Rigidbody _rb; //声明一个刚体组件，用于处理物理效果
+    private int _speed = 300; //定义一个整数变量，用于存储玩家的速度值
     
     public void Start()
     {
-        Debug.Log("测试游戏开始。");  
+         
     }
 
     
-    public void Update()
+    public void FixedUpdate()
     {
-        Debug.Log("测试游戏更新。");
+        _rb.AddForce(0, 0, _speed * Time.deltaTime); //在每个固定更新帧中，向刚体施加一个沿Z轴方向的力，使玩家前进，力的大小为_speed * Time.deltaTime，确保在不同帧率下速度保持一致
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GoLeft(); //调用GoLeft方法，使玩家能够向左移动
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            GoRight(); //调用GoRight方法，使玩家能够向右移动
+        }
+        
+        
     }
+
+    private void GoLeft()
+    {
+        _rb.AddForce(-_speed * Time.deltaTime, 0, 0); //向刚体施加一个沿X轴负方向的力，使玩家向左移动，力的大小为_speed * Time.deltaTime
+    }
+
+    private void GoRight()
+    {
+        _rb.AddForce(_speed * Time.deltaTime, 0, 0); //向刚体施加一个沿X轴正方向的力，使玩家向右移动，力的大小为_speed * Time.deltaTime
+    }   
 }
