adb6ea446d50c455ee080b362e544e7731ebd3d2
553920947 <553920947@qq.com>
2026-08-05 19:15:19 +0800

    新加障碍物

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rb; //声明一个刚体组件，用于处理物理效果
-    private int _speed = 10; //定义一个整数变量，用于存储玩家的速度值
+    private int _speedForward = 10; //定义一个整数变量，用于存储玩家的速度值
+    private int _speedSide = 5; 
     
     public void Start()
     {
@@ -18,7 +19,7 @@ public class Player : MonoBehaviour
     
     public void FixedUpdate()
     {
-        transform.Translate(Vector3.forward * _speed * Time.fixedDeltaTime); //使玩家沿着Z轴正方向移动，移动的距离为_speed * Time.fixedDeltaTime
+        transform.Translate(Vector3.forward * _speedForward * Time.fixedDeltaTime); //使玩家沿着Z轴正方向移动，移动的距离为_speedForward * Time.fixedDeltaTime
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -34,11 +35,11 @@ public class Player : MonoBehaviour
 
     private void GoLeft()
     {
-        transform.Translate(Vector3.left * _speed * Time.fixedDeltaTime); //使玩家沿着X轴负方向移动，移动的距离为_speed * Time.fixedDeltaTime
+        transform.Translate(Vector3.left * _speedSide * Time.fixedDeltaTime); //使玩家沿着X轴负方向移动，移动的距离为_speedSide * Time.fixedDeltaTime
     }
 
     private void GoRight()
     {
-        transform.Translate(Vector3.right * _speed * Time.fixedDeltaTime); //使玩家沿着X轴正方向移动，移动的距离为_speed * Time.fixedDeltaTime
+        transform.Translate(Vector3.right * _speedSide * Time.fixedDeltaTime); //使玩家沿着X轴正方向移动，移动的距离为_speedSide * Time.fixedDeltaTime
     }   
 }
