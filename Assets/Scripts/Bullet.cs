244699904e50eeb82d73756e51ff47e637ff2dc7
553920947 <553920947@qq.com>
2026-09-02 22:00:32 +0800

    添加子弹消除敌人

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,6 +4,15 @@ public class Bullet : MonoBehaviour
 {
     private void Update()
     {
-        transform.Translate(Vector3.forward * 30 * Time.deltaTime); // 子弹速度和持续时间
+        transform.Translate(Vector3.forward * 50 * Time.deltaTime); // 子弹速度和持续时间
+    }
+
+    private void OnCollisionEnter(Collision collision) // 枪射出的子弹碰撞到敌人后销毁
+    {
+        if (collision.gameObject.tag == "Enemy") // 枪射出的子弹碰撞时检查标签Enemy
+        {
+            Destroy(collision.gameObject); // 枪射出的子弹碰撞到敌人后销毁
+        }
+        Destroy(gameObject); // 枪射出的子弹碰撞到敌人后销毁，敌人后销毁
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,4 +6,5 @@ public class Enemy : MonoBehaviour
     {
         transform.Translate(Vector3.forward * 5 * Time.deltaTime); // 敌人速度和持续时间
     }
+
 }
